110fa18de4b7aeeffcfd3bafcada07dd4a4b4914
JohnnyUtah666 <porksloppp@gmail.com>
2022-07-28 11:57:48 -0700

    write function for getComputerChoice

--- myscript.js
+++ myscript.js
@@ -1,1 +1,13 @@
-fucntion 
+function getComputerChoice() {
+   let x = Math.random();
+
+    if (x >= 0 && x <= .33) {
+        return 'rock';
+    } else if (x >= .34 && x <= .65) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+console.log(getComputerChoice());
